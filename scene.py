00e0ecbc981680987db86a5184842202072832f5
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-06-26 12:17:08 -0400

    using animate to animate methods

--- scene.py
+++ scene.py
@@ -1,1 +1,17 @@
 from manim import *
+
+#* using `.animate` syntax to animate methods
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
